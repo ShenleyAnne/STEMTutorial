eae45af6af6ff68aa46e8da483d80e150559ca6a
Shenley Gallimore <Shenley@shenleysmacbook.(none)>
2012-11-13 16:54:54 +0000

    Initial Commit

---
+++ STEMTutorial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  STEMTutorial
+//
+//  Created by Shenley Gallimore on 13/11/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ STEMTutorial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  STEMTutorial
+//
+//  Created by Shenley Gallimore on 13/11/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
